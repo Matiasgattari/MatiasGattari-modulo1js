48644b987b7d9ba970abf8e1a3016bddf3c29cbe
daravhel <lea.mg90@gmail.com>
2022-07-04 21:03:13 -0300

    cambio hoja de registro con validacion

--- js/registro.js
+++ js/registro.js
@@ -5,8 +5,41 @@ function Persona(nombre, edad, direccion, mail, telefono) {
     this.direccion = direccion;
     this.mail = mail;
     this.telefono = telefono;
-}
+
+
+    // Validacion de los datos al momento de la toma 
+    while (!isNaN(nombre)) {
+        alert("Nombre invalido");
+        nombre = prompt("ingresa tu nombre").toLocaleLowerCase();
+    }
+
+    while (!isNaN(direccion)) {
+        alert("direccion no valida");
+        direccion = prompt("Cuál es tu dirección?").toLocaleLowerCase();
+
+    }
+
+    while (!isNaN(mail)) {
+        alert("mail invalido");
+        mail = prompt("Decime tu mail").toLocaleLowerCase();
+
+    }
+
+    while (isNaN(edad)) {
+        alert("La edad debe ser un numero");
+        edad = parseInt(prompt("Ingresa tu edad"));
+    }
+    while (isNaN(telefono)) {
+        alert("El telefono no debe tener puntos ni guiones, numerico");
+        telefono = parseInt(prompt("Ingresa tu telefono"));
+    }
+
+   }
+
 
 const persona1 = new Persona(prompt("ingresa tu nombre").toLocaleLowerCase(), parseInt(prompt("Ingresa tu edad")), prompt("Cuál es tu dirección?").toLocaleLowerCase(), prompt("Decime tu mail").toLocaleLowerCase(), parseInt(prompt("Por último dame tu telefono.")));
-console.log(persona1);
 alert("Registro completo");
+
+
+// Validación de los datos por consola
+console.log(persona1);
